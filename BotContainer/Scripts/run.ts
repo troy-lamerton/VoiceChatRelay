28c2bdc71b893f379a2452731cc69687c4497e12
Troy Lamerton <troy.lamerton@gmail.com>
2019-05-09 13:54:43 +0200

    crossover gave same results as wine - vivox erro 5044 no auith stanza wtf is that? give up

--- BotContainer/Scripts/run.ts
+++ BotContainer/Scripts/run.ts
@@ -3,8 +3,8 @@ import sh from 'shelljs'
 // const display = ':99'
 // sh.exec(`docker kill $(docker ps -q) & docker run --rm -e DISPLAY=${display} voice-relay:linux`)
 
-const ports = `-p ${[6250, 5060, 5062, '12000-12010'].map(a => `${a}:${a}`).join(' -p ')}`
-const command = `docker run --rm voice-relay:linux`
+const ports = `-p ${[6080, 5901].map(a => `${a}:${a}`).join(' -p ')}`
+const command = `docker run --name crossover-vnc ${ports} voice-relay:linux`
 
 console.log(command)
 
